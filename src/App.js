8f1dde9df8c162155f95e65677d121b3123bd50a
rakesh-i <rakesh.007.ac@gmail.com>
2023-11-16 19:42:30 +0530

    basic format

--- src/App.js
+++ src/App.js
@@ -2,80 +2,76 @@ import back from './background.jpg'
 import './App.css';
 import logo from './odin-lined.png'
 
-
 function Sidebar(){
   return(
-    <>
-      <div className='sidebar'>
-        {/* <img src={back} alt="background" /> */}
-        <div className='logo'>
-          <img src={logo} alt="logo" />
-          <p>ODIN</p>
-        </div>
+    <div className='sidebar'>
+      <div className='logo'>
+        <img src={logo} alt="logo" />
+        <p>ODIN</p>
       </div>
-    </>
+      <div className="credit">
+        <p>Photo by <a href="https://unsplash.com/@haliewestphoto">Halie West</a> on <a href="https://unsplash.com/photos/green-leaf-plant-in-close-up-photography-25xggax4bSA">Upsplash</a></p>
+      </div>
+    </div>
   );
 }
 
 function Content(){
   return(
-    <div className='left-side'>
-    <div className='top'>
-      <p>
-        This is not a real online service! You know you need something line this in your life to help you realize your deepest dreams. Signup <em>now</em> to get started.
-      </p>
-      <p>You <em>know</em> you want to.</p>
-    </div>
-    <div className='data'>
-      <form action="">
-        <div className='middle'>
-          <p className='header'>Let's do this!</p>
-          <div className='info'>
-            <div className='col1'>
-              <div>
+    <div className='content'>
+      <div className="header">
+        <p>
+          This is not a real online service! You know you need something like this in your life to help you to realize your deepest dreams. Sign up <em>now</em> to get started.
+        </p>
+        <p>You <em>know</em> what you want</p>
+      </div>
+      <div className="middle">
+        <div className="container">
+        <form action="">
+          <div id='legend'><p>Let's do this!</p></div>
+          <div className='form'>
+            <div className='flex-col'>
+              <div id="form-ele">
                 <label For="first">FIRST NAME</label>
-                <input type="text" name="first" id='first' required/>
+                <input type="text" name="first" id="first" />
               </div>
-              <div>
+              <div id="form-ele">
                 <label For="mail">EMAIL</label>
-                <input type="email" name='mail' id='mail' required/>
+                <input type="mail" name="mail" id="mail" />
               </div>
-              <div>
+              <div id="form-ele">
                 <label For="pwd">PASSWORD</label>
-                <input type="password" name="pwd" id='pwd' required/>
+                <input type="password" name="pwd" id="pwd" />
               </div>
             </div>
-            <div className='col2'>
-              <div>
+            <div className='flex-col'>
+              <div id="form-ele">
                 <label For="last">LAST NAME</label>
-                <input type="text" name='last' id='last' required/>
+                <input type="text" name="last" id="last" />
               </div>
-              <div>
+              <div id="form-ele">
                 <label For="phone">PHONE NUMBER</label>
-                <input type="number" name='phone' id='phone' required/>
+                <input type="number" name="phone" id="phone" />
               </div>
-              <div>
-                <label For="pwdc">CONFIRM PASSWORD</label>
-                <input type="password" name='pwdc' id='pwdc' required/>
+              <div id="form-ele">
+                <label For="cpwd">CONFIRM PASSWORD</label>
+                <input type="pwd" name="cpwd" id="cpwd" />
               </div>
             </div>
           </div>
+          <button>Create Account</button>
+          </form>
         </div>
-        <button type="submit" id='create'>Create Account</button>
-      </form>
-    </div>
-    <div className='already'>
-      <p>Already have an account?<span><button id='login'>Log in</button></span></p>
-    </div>
+      </div>
     </div>
   );
 }
 
 function App() {
   return (
-    <div className='page'>
+    <div className='app'>
       <Sidebar/>
-      <Content />
+      <Content/>
     </div>
   );
 }
