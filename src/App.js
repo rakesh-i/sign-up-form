0e6354b2cc397fc21f1c1e9cf33ac4dedcd0c64d
rakesh-i <rakesh.007.ac@gmail.com>
2023-11-23 13:20:55 +0530

    till password validation

--- src/App.js
+++ src/App.js
@@ -20,52 +20,56 @@ function Content(){
   return(
     <div className='content'>
       <div className="header">
-        <p>
-          This is not a real online service! You know you need something like this in your life to help you to realize your deepest dreams. Sign up <em>now</em> to get started.
-        </p>
+        <p>This is not a real online service! You know you need something like this in your life to help you to realize your deepest dreams. Sign up <em>now</em> to get started.</p>
         <p>You <em>know</em> what you want</p>
       </div>
-      <div className="middle">
-        <div className="container">
-        <form action="">
-          <div id='legend'><p>Let's do this!</p></div>
-          <div className='form'>
-            <div className='flex-col'>
-              <div id="form-ele">
-                <label For="first">FIRST NAME</label>
-                <input type="text" name="first" id="first" />
+      <div className="container">
+        <div className='middle'>
+          <div id='legend'>Let's do this!</div>
+          <form id='myForm'>
+              <div className='flex-col'>
+                <div id="form-ele">
+                  <label For="first">FIRST NAME</label>
+                  <input type="text" name="first" id="first" required/>
+                </div>
+                <div id="form-ele">
+                  <label For="mail">EMAIL</label>
+                  <input type="mail" name="mail" id="mail" required/>
+                </div>
+                <div id="form-ele">
+                  <label For="pwd">PASSWORD</label>
+                  <input type="password" name="pwd" id="pwd" required/>
+                </div>
               </div>
-              <div id="form-ele">
-                <label For="mail">EMAIL</label>
-                <input type="mail" name="mail" id="mail" />
+              <div className='flex-col'>
+                <div id="form-ele">
+                  <label For="last">LAST NAME</label>
+                  <input type="text" name="last" id="last" required/>
+                </div>
+                <div id="form-ele">
+                  <label For="phone">PHONE NUMBER</label>
+                  <input type="number" name="phone" id="phone" required/>
+                </div>
+                <div id="form-ele">
+                  <label For="cpwd">CONFIRM PASSWORD</label>
+                  <input type="password" name="cpwd" id="cpwd" required/>
+                </div>
               </div>
-              <div id="form-ele">
-                <label For="pwd">PASSWORD</label>
-                <input type="password" name="pwd" id="pwd" />
-              </div>
-            </div>
-            <div className='flex-col'>
-              <div id="form-ele">
-                <label For="last">LAST NAME</label>
-                <input type="text" name="last" id="last" />
-              </div>
-              <div id="form-ele">
-                <label For="phone">PHONE NUMBER</label>
-                <input type="number" name="phone" id="phone" />
-              </div>
-              <div id="form-ele">
-                <label For="cpwd">CONFIRM PASSWORD</label>
-                <input type="pwd" name="cpwd" id="cpwd" />
-              </div>
-            </div>
-          </div>
-          <button>Create Account</button>
           </form>
         </div>
       </div>
+      <div className="create-account">
+        <button id="create" type='submit' form='myForm'>Create Account</button>
+      </div>
+      <div className="login">
+        <p>Already have an account?<a href="">Log in</a></p>
+      </div>
+      
     </div>
   );
 }
+
+
 
 function App() {
   return (
